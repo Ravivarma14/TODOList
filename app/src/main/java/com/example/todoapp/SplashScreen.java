97236b5d6f1209818cbf34f3743c8a5eb28e6ac2
Ravivarma <ravi.varma.madasu.2@gmail.com>
2024-01-24 18:26:29 +0530

    SplashScreen done

--- app/src/main/java/com/example/todoapp/SplashScreen.java
+++ app/src/main/java/com/example/todoapp/SplashScreen.java
@@ -2,7 +2,9 @@ package com.example.todoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        final Intent i=new Intent(SplashScreen.this,MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        },1000);
     }
 }
